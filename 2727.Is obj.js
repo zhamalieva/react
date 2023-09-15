178e21d3bed7ebb9ebea4a643a802364d6657c07
linlifxee <88711112+zhamalieva@users.noreply.github.com>
2023-09-16 01:21:23 +0600

    Create 2727.Is obj.js

---
+++ 2727.Is obj.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if (typeof(obj) == 'object') {
+        return (Object.keys(obj).length == 0) ;
+    }
+};
+    
