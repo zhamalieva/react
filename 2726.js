d9a94442040433153b42422e3aa7e36889fec030
linlifxee <88711112+zhamalieva@users.noreply.github.com>
2023-09-16 12:47:12 +0600

    Create 2726.js

---
+++ 2726.js
@@ -0,0 +1,66 @@
+lass Calculator {
+  
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.result = value 
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        this.result += value
+        return this
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        this.result -= value
+        return this
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.result *= value
+        return this
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if( value !== 0){
+          this.result /= value
+        }
+        else{
+            this.result = "Division by zero is not allowed"
+        }
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.result = this.result ** value
+        return this
+    }
+      
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.result
+    }
+  }
